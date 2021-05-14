b6994e2ee8f3cf40dfe4a8edb26c5e4396337b85
chillmouse <koftun251201@inbox.ru>
2021-05-14 23:59:13 +0700

    Update

---
+++ cmake-build-debug/CMakeFiles/C_rocketBeans_work3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_rocketBeans_work3.dir/event5.c.obj"
+  "C_rocketBeans_work3.exe"
+  "C_rocketBeans_work3.exe.manifest"
+  "C_rocketBeans_work3.pdb"
+  "libC_rocketBeans_work3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_rocketBeans_work3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
